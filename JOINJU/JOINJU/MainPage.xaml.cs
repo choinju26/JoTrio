1a6c03f4c80ea7fcca06bba046fdaa21198e65a6
쓰레쉬는강력합니다 쓰레쉬는강력합니다 <tange3232@gmail.com>
2019-01-14 22:13:09 +0900

    commit

---
+++ JOINJU/JOINJU/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Windows.Input;
+
+namespace JOINJU
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            NavigateCommand = new Command<Type>(async (Type pageType) =>
+            {
+                Page page = (Page)Activator.CreateInstance(pageType);
+                await Navigation.PushAsync(page);
+            });
+
+            BindingContext = this;
+        }
+
+        public ICommand NavigateCommand { private set; get; }
+    }
+}
