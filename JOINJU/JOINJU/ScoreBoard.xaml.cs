49d51f876c9a94012c8b6a5d93bd5d2070fd56fc
kasarea2 <38400316+kasarea2@users.noreply.github.com>
2019-01-24 23:03:10 +0900

    스코어보드 업데이트 . 투명버튼 추가로 스코어 업 다운 가능

--- JOINJU/JOINJU/ScoreBoard.xaml.cs
+++ JOINJU/JOINJU/ScoreBoard.xaml.cs
@@ -13,15 +13,18 @@ namespace JOINJU
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ScoreBoard : ContentPage
 	{
-		public ScoreBoard ()
-		{
+        //lbRedTeam = this.FindByName<Label>("lbRedTeam");
+        int redScore = 0;
+        int blueScore = 0;
+        public ScoreBoard ()
+        {
             InitializeComponent ();
-		}
+        }
         protected override void OnAppearing()
         {
             base.OnAppearing();
             MessagingCenter.Send(this, "allowLandScapePortrait");
-
+            
             //Debug.Print("스코어보드 진입");
         }
         //during page close setting back to portrait
@@ -31,6 +34,50 @@ namespace JOINJU
             MessagingCenter.Send(this, "preventLandScape");
 
             //Debug.Print("스코어보드 나가기");
+        }
+
+        //적팀 스코어 UP 이벤트
+        public void RedTeam_ScoreUp(object sender , EventArgs e)
+        {
+            if(redScore + 1 <= 100)
+            {
+                redScore++;
+            }
+            lbRedTeam.Text = string.Format("{0}",redScore);
+            Debug.Print("적팀 스코어 업"+ redScore);
+        }
+
+        //적팀 스코어 Down 이벤트
+        public void RedTeam_ScoreDown(object sender, EventArgs e)
+        {
+            if (redScore - 1 >= 0)
+            {
+                redScore--;
+            }
+            lbRedTeam.Text = string.Format("{0}",redScore);
+            Debug.Print("적팀 스코어 다운" + redScore);
+        }
+
+        //청팀 스코어 UP 이벤트
+        public void BlueTeam_ScoreUp(object sender, EventArgs e)
+        {
+            if (blueScore + 1 <= 100)
+            {
+                blueScore++;
+            }
+            lbBlueTeam.Text = string.Format("{0}",blueScore);
+            Debug.Print("청팀 스코어 업" + redScore);
+        }
+
+        //청팀 스코어 Down 이벤트
+        public void BlueTeam_ScoreDown(object sender, EventArgs e)
+        {
+            if (blueScore - 1 >= 0)
+            {
+                blueScore--;
+            }
+            lbBlueTeam.Text = string.Format("{0}",blueScore);
+            Debug.Print("청팀 스코어 다운" + redScore);
         }
     }
 }
